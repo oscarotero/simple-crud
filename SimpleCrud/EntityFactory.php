72d30211c1104c98bc963fc9fc0ba05399b0535a
Oscar Otero <oom@oscarotero.com>
2013-11-08 11:41:29 +0100

    Added parameter types to entities

--- SimpleCrud/EntityFactory.php
+++ SimpleCrud/EntityFactory.php
@@ -7,6 +7,8 @@
 
 namespace SimpleCrud;
 
+use PDO;
+
 class EntityFactory {
 	protected $manager;
 	protected $namespace;
@@ -40,7 +42,21 @@ class EntityFactory {
 		$entity = new $class($this->manager, $name);
 
 		$table = empty($entity->table) ? $name : $entity->table;
-		$fields = empty($entity->fields) ? $this->manager->execute("DESCRIBE `$table`")->fetchAll(\PDO::FETCH_COLUMN, 0) : $entity->fields;
+
+		if (empty($entity->fields)) {
+			$fields = $this->getFields($table);
+		} else {
+			$fields = [];
+
+			foreach ($entity->fields as $k => $field) {
+				if (is_int($k)) {
+					$fields[$field] = PDO::PARAM_STR;
+				} else {
+					$fields[$k] = $field;
+				}
+			}
+		}
+
 		$foreignKey = empty($entity->foreignKey) ? "{$table}_id" : $entity->foreignKey;
 		$rowClass = class_exists("{$class}Row") ? "{$class}Row" : 'SimpleCrud\\Row';
 		$rowCollectionClass = class_exists("{$class}RowCollection") ? "{$class}RowCollection" : 'SimpleCrud\\RowCollection';
@@ -50,5 +66,32 @@ class EntityFactory {
 		$entity->onInit();
 
 		return $entity;
+	}
+
+	protected function getFields ($table) {
+		$tmp_fields = $this->manager->execute("DESCRIBE `$table`")->fetchAll();
+		$fields = [];
+
+		foreach ($tmp_fields as $field) {
+			preg_match('#^(\w+)#', $field['Type'], $matches);
+
+			switch ($matches[1]) {
+				case 'tinyint':
+				case 'smallint':
+				case 'mediumint':
+				case 'int':
+				case 'bigint':
+					$type = PDO::PARAM_INT;
+					break;
+				
+				default:
+					$type = PDO::PARAM_STR;
+					break;
+			}
+
+			$fields[$field['Field']] = $type;
+		}
+
+		return $fields;
 	}
 }
