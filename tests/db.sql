6f547d6b5734564cc66daded2b334ccb13790b0f
Óscar Otero <oom@oscarotero.com>
2013-12-31 19:13:32 +0100

    added db.sql

--- tests/db.sql
+++ tests/db.sql
@@ -1,1 +1,68 @@
-Ctrl-C -- exit!
+--
+-- Estrutura da táboa `categories`
+--
+
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da táboa `posts`
+--
+
+CREATE TABLE IF NOT EXISTS `posts` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `categories_id` int(11) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `categories_id` (`categories_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da táboa `tags`
+--
+
+CREATE TABLE IF NOT EXISTS `tags` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da táboa `tags_in_posts`
+--
+
+CREATE TABLE IF NOT EXISTS `tags_in_posts` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `posts_id` int(11) unsigned NOT NULL,
+  `tags_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `posts_id` (`posts_id`),
+  KEY `tags_id` (`tags_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+--
+-- Restricións para os envorcados das táboas
+--
+
+--
+-- Restricións para a táboa `posts`
+--
+ALTER TABLE `posts`
+  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`categories_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE;
+
+--
+-- Restricións para a táboa `tags_in_posts`
+--
+ALTER TABLE `tags_in_posts`
+  ADD CONSTRAINT `tags_in_posts_ibfk_2` FOREIGN KEY (`tags_id`) REFERENCES `tags` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `tags_in_posts_ibfk_1` FOREIGN KEY (`posts_id`) REFERENCES `posts` (`id`) ON DELETE CASCADE;
