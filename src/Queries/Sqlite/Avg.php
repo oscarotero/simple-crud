2465415c1c67aa8d510f20aa268fdecf6c32b1d6
andrenam <andrenam.berger@gmail.com>
2017-11-25 22:25:41 +0100

    add avg query

---
+++ src/Queries/Sqlite/Avg.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace SimpleCrud\Queries\Sqlite;
+
+use SimpleCrud\Queries\Mysql\Max as BaseAvg;
+
+/**
+ * Manages a database max query in Sqlite databases.
+ */
+class Avg extends BaseAvg
+{
+}
